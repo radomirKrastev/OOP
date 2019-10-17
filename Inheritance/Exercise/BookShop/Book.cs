ffbcd1c9cdd6c58509dcd9caece0b24e29c951b6
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-17 15:15:52 +0300

    Inheritance exercise

---
+++ Inheritance/Exercise/BookShop/Book.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+namespace BookShop
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+        private decimal price;
+
+        public Book(string author, string title, decimal price)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Price = price;
+        }
+
+        public string Title
+        {
+            get => this.title;
+            protected set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Title not valid!");
+                }
+
+                this.title = value;
+            }
+        }
+
+        public string Author
+        {
+            get => this.author;
+            protected set
+            {
+                var authorName = value.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                if (char.IsDigit(authorName[1][0]))
+                {
+                    throw new ArgumentException("Author not valid!");
+                }
+
+                this.author = value;
+            }
+        }
+
+        public virtual decimal Price
+        {
+            get => this.price;
+            protected set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Price not valid!");
+                }
+
+                this.price = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            var output = new StringBuilder();
+            output.AppendLine($"Type: {this.GetType().Name}")
+                .AppendLine($"Title: {this.Title}")
+                .AppendLine($"Author: {this.Author}")
+                .AppendLine($"Price: {this.Price:f2}");
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
