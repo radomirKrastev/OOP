0f27395ce24479117c245575aea9532dfbb1db84
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-17 09:52:30 +0300

    CustomRandomList

---
+++ Inheritance/Lab/CustomRandomList/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomRandomList
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var customList = new RandomList() { "Petio", "Gosho", "Apostol Popov", "Kircata" };
+            Console.WriteLine(customList.RandomString());
+        }
+    }
+}
