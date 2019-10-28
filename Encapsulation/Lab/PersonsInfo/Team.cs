811fbf412bb881c05835a41945b19cfa3450fbe3
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-28 17:35:56 +0200

    Encapsulation problem from lab final state

---
+++ Encapsulation/Lab/PersonsInfo/Team.cs
@@ -0,0 +1,33 @@
+﻿namespace PersonsInfo
+{
+    using System.Collections.Generic;
+    
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+        public IReadOnlyList<Person> FirstTeam => this.firstTeam.AsReadOnly();
+        
+        public IReadOnlyList<Person> ReserveTeam => this.reserveTeam.AsReadOnly();
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                this.firstTeam.Add(person);
+            }
+            else
+            {
+                this.reserveTeam.Add(person);
+            }
+        }
+    }
+}
