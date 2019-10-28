d197f2edc4c155599d56b7b9905204c6ddd7fa05
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-28 18:23:32 +0200

    Encapsulate box

---
+++ Encapsulation/Exercise/ClassBox/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ClassBox
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var length = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                var box = new Box(length, width, height);
+
+                Console.WriteLine($"Surface Area - {box.GetSurfaceArea():F2}");
+                Console.WriteLine($"Lateral Surface Area - {box.GetLateralSurfaceArea():F2}");
+                Console.WriteLine($"Volume - {box.GetVolume():F2}");
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+        }
+    }
+}
