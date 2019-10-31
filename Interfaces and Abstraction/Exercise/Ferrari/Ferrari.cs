aa0bd7c1585f84fa71ca949927bc95aa46a84b45
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-31 22:13:10 +0200

    interfaces and abstraction

---
+++ Interfaces and Abstraction/Exercise/Ferrari/Ferrari.cs
@@ -0,0 +1,28 @@
+﻿namespace Ferrari
+{
+    public class Ferrari : ICar
+    {
+        private string model = "488-Spider";
+        private string driver;
+
+        public Ferrari(string driver)
+        {
+            this.driver = driver;
+        }
+        
+        public string PressTheGas()
+        {
+            return "Gas!";
+        }
+
+        public string UseBrakes()
+        {
+            return "Brakes!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.model}/{this.UseBrakes()}/{this.PressTheGas()}/{this.driver}";
+        }
+    }
+}
---
+++ Interfaces and Abstraction/Exercise/Ferrari/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ferrari
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var driver = Console.ReadLine();
+            ICar ferrari = new Ferrari(driver);
+            Console.WriteLine(ferrari.ToString());
+        }
+    }
+}
