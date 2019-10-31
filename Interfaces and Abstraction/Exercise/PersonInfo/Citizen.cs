99cb26b6f7791750a74aba58818df145707320ab
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-31 22:13:37 +0200

    interfaces and abstraction

---
+++ Interfaces and Abstraction/Exercise/PersonInfo/Citizen.cs
@@ -0,0 +1,31 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        private string name;
+        private int age;
+        private string id;
+        private string birthdate;
+
+        public Citizen(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public Citizen(string name, int age, string id, string birthdate)
+            : this(name, age)
+        {
+            this.id = id;
+            this.birthdate = birthdate;
+        }
+
+        public string Name => this.name;
+         
+        public int Age => this.age;
+
+        public string Birthdate => this.birthdate;
+
+        public string Id => this.id;
+    }
+}
