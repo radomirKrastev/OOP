3dd6ea8741ec8725bb50de751dd814451d0c4c92
radomirKrastev <radomirkostantinov@gmail.com>
2019-10-31 22:13:53 +0200

    interfaces and abstraction

---
+++ Interfaces and Abstraction/Exercise/Telephony/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Telephony
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split();
+
+            var smartphone = new Smartphone();
+
+            foreach (var number in numbers)
+            {
+                try
+                {
+                    Console.WriteLine(smartphone.Call(number)); 
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+            }
+        
+            var sites = Console.ReadLine().Split();
+
+            foreach (var site in sites)
+            {
+                try
+                {
+                    Console.WriteLine(smartphone.Browse(site)); 
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Interfaces and Abstraction/Exercise/Telephony/Smartphone.cs
@@ -0,0 +1,28 @@
+﻿namespace Telephony
+{
+    using System;
+    using System.Linq;
+
+    public class Smartphone : IBrowser, ICaller
+    {
+        public string Browse(string site)
+        {
+            if (site.Any(char.IsDigit))
+            {
+                throw new ArgumentException("Invalid URL!");
+            }
+
+            return $"Browsing: {site}!";
+        }
+
+        public string Call(string number)
+        {
+            if (number.All(char.IsNumber))
+            {
+                return $"Calling... {number}";
+            }
+
+            throw new ArgumentException("Invalid number!");
+        }
+    }
+}
