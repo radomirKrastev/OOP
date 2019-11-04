578de9e7e425374e86eb22bf0a17f4aa8b59b532
radomirKrastev <radomirkostantinov@gmail.com>
2019-11-04 19:47:53 +0200

    explixit interfaces

---
+++ Interfaces and Abstraction/Exercise/ExplicitInterfaces/Citizen.cs
@@ -0,0 +1,32 @@
+﻿namespace ExplicitInterfaces
+{
+    public class Citizen : IPerson, IResident
+    {
+        private string name;
+        private string country;
+        private int age;
+
+        public Citizen(string name, string country, int age)
+        {
+            this.name = name;
+            this.country = country;
+            this.age = age;
+        }
+
+        public string Name => this.name;
+
+        public int Age => this.age;
+
+        public string Country => this.country;
+
+        string IPerson.GetName()
+        {
+            return this.Name;
+        }
+
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {this.Name}";
+        }
+    }
+}
---
+++ Interfaces and Abstraction/Exercise/ExplicitInterfaces/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ExplicitInterfaces
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                var commandParts = command.Split(" ");
+
+                var name = commandParts[0];
+                var country = commandParts[1];
+                var age = int.Parse(commandParts[2]);
+
+                IPerson person = new Citizen(name, country, age);
+                IResident resident = new Citizen(name, country, age);
+                
+                Console.WriteLine(person.GetName());
+                Console.WriteLine(resident.GetName());
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
