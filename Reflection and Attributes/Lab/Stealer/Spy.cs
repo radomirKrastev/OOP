99b7503c67d627a7bd6a7e0cd463806cf99ad40f
radomirKrastev <radomirkostantinov@gmail.com>
2019-11-15 12:41:18 +0200

    Logger Library

---
+++ Reflection and Attributes/Lab/Stealer/Spy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+public class Spy
+{
+    public string StealFieldInfo(string className, params string[] fieldsNames)
+    {
+        Type classType = Type.GetType(className);
+
+        FieldInfo[] classFields = classType.GetFields(
+            BindingFlags.Public
+            | BindingFlags.NonPublic
+            | BindingFlags.Instance
+            | BindingFlags.Static);
+
+        var output = new StringBuilder();
+
+        object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+        output.AppendLine($"Class under investigation: {className}");
+
+        foreach (FieldInfo field in classFields.Where(f => fieldsNames.Contains(f.Name)))
+        {
+            output.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+        }
+
+        return output.ToString().TrimEnd();
+    }
+}
+
