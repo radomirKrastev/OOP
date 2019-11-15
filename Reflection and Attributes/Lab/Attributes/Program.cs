d9bcf5ea2ed9f6f3083ded160198fd42370f4b51
radomirKrastev <radomirkostantinov@gmail.com>
2019-11-15 15:05:04 +0200

    attributes

---
+++ Reflection and Attributes/Lab/Attributes/Program.cs
@@ -0,0 +1,11 @@
+﻿[Author ("Ventsi")]
+
+public class Program
+{
+    [Author("Gosho")]
+    public static void Main()
+    {
+        var tracker = new Tracker();
+        tracker.PrintMethodsByAuthor();
+    }
+}
---
+++ Reflection and Attributes/Lab/Attributes/Tracker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+public class Tracker
+{
+    public void PrintMethodsByAuthor()
+    {
+        Type type = typeof(Program);
+
+        MethodInfo[] methods = type.GetMethods(
+            BindingFlags.Static 
+            | BindingFlags.Instance 
+            | BindingFlags.Public 
+            | BindingFlags.NonPublic);
+
+        foreach (var method in methods)
+        {
+            if (method.CustomAttributes.Any(a => a.AttributeType == typeof(AuthorAttribute)))
+            {
+                var attributes = method.GetCustomAttributes();
+
+                foreach (AuthorAttribute attribute in attributes)
+                {
+                    Console.WriteLine("{0} is written by {1}", method.Name, attribute.Name);
+                }                
+            }
+        }
+    }
+}
