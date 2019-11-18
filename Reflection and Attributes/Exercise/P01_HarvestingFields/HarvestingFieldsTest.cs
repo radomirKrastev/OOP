b268ace810cf1f836f599c5376e725e4baf290cf
radomirKrastev <radomirkostantinov@gmail.com>
2019-11-18 10:34:47 +0200

    reflection

---
+++ Reflection and Attributes/Exercise/P01_HarvestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,54 @@
+﻿ namespace P01_HarvestingFields
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+            Type classType = typeof(HarvestingFields);
+
+            var command = Console.ReadLine();
+
+            while (command != "HARVEST")
+            {
+                FieldInfo[] allFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+                switch (command)
+                {
+                    case "private":
+                        PrintFields(allFields.Where(x=>x.IsPrivate).ToArray());
+                        break;
+                    case "public":
+                        PrintFields(allFields.Where(x => x.IsPublic).ToArray());
+                        break;
+                    case "protected":
+                        PrintFields(allFields.Where(x => x.IsFamily).ToArray());
+                        break;
+                    default:
+                        PrintFields(allFields);
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+
+        private static void PrintFields(FieldInfo[] fields)
+        {
+            StringBuilder output = new StringBuilder();
+
+            foreach (FieldInfo fieldInfo in fields)
+            {
+                output.AppendLine($"{fieldInfo.Attributes.ToString().ToLower()} " +
+                    $"{fieldInfo.FieldType.Name} " +
+                    $"{fieldInfo.Name}");
+            }
+
+            Console.WriteLine(output.ToString().TrimEnd()); 
+        }
+    }
+}
