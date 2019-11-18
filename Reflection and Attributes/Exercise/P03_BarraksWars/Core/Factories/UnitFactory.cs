9494e5820c966035355d8a6b0534f891b071c17b
radomirKrastev <radomirkostantinov@gmail.com>
2019-11-18 20:26:49 +0200

    simple factory

---
+++ Reflection and Attributes/Exercise/P03_BarraksWars/Core/Factories/UnitFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace _03BarracksFactory.Core.Factories
+{
+    using System;
+    using Contracts;
+
+    public class UnitFactory : IUnitFactory
+    {
+        public IUnit CreateUnit(string unitType)
+        {
+            var unitClassPath = $"{typeof(AppEntryPoint).Namespace}.Models.Units.";
+
+            Type classType = Type.GetType(unitClassPath + unitType);
+
+            IUnit unit = (IUnit)Activator.CreateInstance(classType);
+
+            return unit;
+        }
+    }
+}
