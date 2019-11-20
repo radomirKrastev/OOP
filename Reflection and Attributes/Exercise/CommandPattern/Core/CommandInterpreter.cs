a2c7f40afdf3ae13f24ee57a2472a674fc5c93ac
radomirKrastev <radomirkostantinov@gmail.com>
2019-11-20 11:51:12 +0200

    Reflections

---
+++ Reflection and Attributes/Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,32 @@
+﻿namespace CommandPattern.Core
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using Contracts;
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            var commandExtension = "Command";
+
+            var commandParts = args
+                .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+            var commandName = commandParts[0] + commandExtension;
+            var commandData = commandParts.Skip(1).ToArray();
+
+            var commandType = Assembly
+                .GetCallingAssembly()
+                .GetTypes()
+                .FirstOrDefault(x => x.Name == commandName);
+
+            var commandInstance = (ICommand)Activator.CreateInstance(commandType);
+
+            var result = commandInstance.Execute(commandData);
+
+            return result.ToString();
+        }
+    }
+}
---
+++ Reflection and Attributes/Exercise/CommandPattern/Core/Engine.cs
@@ -0,0 +1,24 @@
+﻿namespace CommandPattern.Core
+{
+    using System;
+    using Contracts;
+
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                var command = Console.ReadLine();
+                Console.WriteLine(this.commandInterpreter.Read(command));
+            }
+        }
+    }
+}
