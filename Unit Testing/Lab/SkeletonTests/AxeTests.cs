cbcb10ae2cc00e7f25f02b85a3b9c75772e0d380
radomirKrastev <radomirkostantinov@gmail.com>
2019-11-21 23:35:47 +0200

    test dummy methods

--- Unit Testing/Lab/SkeletonTests/AxeTests.cs
+++ Unit Testing/Lab/SkeletonTests/AxeTests.cs
@@ -1,8 +1,8 @@
-using NUnit.Framework;
-using System;
-
 namespace SkeletonTests
 {
+    using System;
+    using NUnit.Framework;
+    
     [TestFixture]
     public class AxeTests
     {
@@ -14,14 +14,14 @@ namespace SkeletonTests
         [Test]
         public void AxeLoosesDurabilityAfterAttack()
         {
-            //Arrange
+            ////Arrange
             Axe axe = new Axe(10, 10);
             Dummy dummy = new Dummy(10, 10);
 
-            //Act
+            ////Act
             axe.Attack(dummy);
 
-            //Assert
+            ////Assert
 
             Assert.AreEqual(9, axe.DurabilityPoints, "Axe Durability doesn't change after attack.");
         }
---
+++ Unit Testing/Lab/SkeletonTests/DummyTests.cs
@@ -0,0 +1,54 @@
+﻿namespace SkeletonTests
+{
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void DummyLoosesHealthWhenAttacked()
+        {
+            Axe axe = new Axe(2, 10);
+            Dummy dummy = new Dummy(10, 10);
+
+            axe.Attack(dummy);
+
+            Assert.AreEqual(8, dummy.Health);
+        }
+
+        [Test]
+        ////[TestCase(2, 0)]
+        ////[TestCase(2, -1)]
+        ////[TestCase(2, 1)]
+        public void DeadDummyThrowsInvalidOperationExceptionWhenAttacked()
+        {
+            Axe axe = new Axe(2, 10);
+            Dummy dummy = new Dummy(0, 10);
+
+            Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+        }
+
+        [Test]
+        ////[TestCase(0)]
+        ////[TestCase(-1)]
+        ////[TestCase(1)]
+        public void DeadDummyGivesExperience()
+        {
+            Dummy dummy = new Dummy(0, 10);
+
+            Assert.AreEqual(10, dummy.GiveExperience());
+        }
+
+        [Test]
+        ////[TestCase(0)]
+        ////[TestCase(-1)]
+        ////[TestCase(1)]
+        public void AliveDummyDoesNotGiveExperienceInsteadShouldThrowInvalidOperationException()
+        {
+            Dummy dummy = new Dummy(1, 10);
+
+            Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+        }
+    }
+}
