852074f92db73f95ad95a5d3be1330075cdad359
radomirKrastev <radomirkostantinov@gmail.com>
2019-11-24 15:48:09 +0200

    unit test FightingArena

--- Unit Testing/Exercise/FightingArena.Tests/ArenaTests.cs
+++ Unit Testing/Exercise/FightingArena.Tests/ArenaTests.cs
@@ -1,18 +1,98 @@
-﻿using NUnit.Framework;
-
-namespace Tests
+﻿namespace FightingArena.Tests
 {
+    using System;
+    using System.Collections.Generic;
+    using NUnit.Framework;    
+
+    [TestFixture]
     public class ArenaTests
     {
+        private Arena arena;
+
         [SetUp]
         public void Setup()
         {
+            this.arena = new Arena();
         }
 
         [Test]
-        public void Test1()
+        public void ArenaConstructorCreatesEmptyWarriorList()
         {
-            Assert.Pass();
+            Assert.IsNotNull(this.arena.Warriors);
+        }
+
+        [Test]
+        public void WarriorsGetterWorksCorrectly()
+        {
+            Warrior warrior = new Warrior("Maximus", 500, 500);
+            this.arena.Enroll(warrior);
+
+            List<Warrior> warriorGetter = (List<Warrior>)this.arena.Warriors;
+
+            Assert.AreEqual(warrior, warriorGetter[0]);
+            Assert.AreEqual(1, this.arena.Count);
+        }
+
+        [Test]
+        public void CountGetterWorksCorrectly()
+        {
+            Warrior warrior = new Warrior("Maximus", 500, 500);
+
+            this.arena.Enroll(warrior);
+
+            Assert.AreEqual(1, this.arena.Count);
+        }
+
+        [Test]
+        public void EnrollFunctionalityAddsWarriorToArenaListCorrectly()
+        {
+            Warrior warrior = new Warrior("Maximus", 500, 500);
+
+            this.arena.Enroll(warrior);
+            List<Warrior> warriorsList = (List<Warrior>)this.arena.Warriors;
+
+            Assert.AreEqual(1, this.arena.Count);
+            Assert.AreEqual(warrior, warriorsList[0]);
+        }
+
+        [Test]
+        public void EnrollWarriorWithAlreadyExistingNameInTheArenaListThrowsInvalidOperationException()
+        {
+            Warrior firstWarrior = new Warrior("Maximus", 500, 500);
+            Warrior secondWarrior = new Warrior("Maximus", 1000, 1000);
+
+            this.arena.Enroll(firstWarrior);
+
+            Assert.Throws<InvalidOperationException>(() => this.arena.Enroll(secondWarrior));
+        }
+
+        [Test]
+        public void FightWorksCorrectlyBothWarriorsAttackEachOtherAndTheirHealthDecrease()
+        {
+            Warrior firstWarrior = new Warrior("Maximus", 100, 100);
+            Warrior secondWarrior = new Warrior("Commodus", 100, 100);
+
+            this.arena.Enroll(firstWarrior);
+            this.arena.Enroll(secondWarrior);
+            this.arena.Fight("Maximus", "Commodus");
+
+            List<Warrior> warriorsList = (List<Warrior>)this.arena.Warriors;
+
+            Assert.AreEqual(0, warriorsList[0].HP);
+            Assert.AreEqual(0, warriorsList[1].HP);
+        }
+
+        [Test]
+        public void FightThrowsInvalidOperationExceptionWhenOneOfTheWarriorsIsNotEnrolled()
+        {
+            Warrior firstWarrior = new Warrior("Maximus", 500, 500);
+            Warrior secondWarrior = new Warrior("Commodus", 1000, 1000);
+            Warrior thirdWarrior = new Warrior("Spartacus", 50000, 50000);
+
+            this.arena.Enroll(firstWarrior);
+            this.arena.Enroll(secondWarrior);
+
+            Assert.Throws<InvalidOperationException>(() => this.arena.Fight("Maximus", "Spartacus"));
         }
     }
 }
--- Unit Testing/Exercise/FightingArena.Tests/WarriorTests.cs
+++ Unit Testing/Exercise/FightingArena.Tests/WarriorTests.cs
@@ -1,18 +1,100 @@
-using NUnit.Framework;
-
-namespace Tests
+namespace FightingArena.Tests
 {
+    using System;
+    using NUnit.Framework;    
+
     public class WarriorTests
     {
+        private Warrior warrior;
+
         [SetUp]
         public void Setup()
         {
+            this.warrior = new Warrior("Maximus", 100, 100);
         }
 
         [Test]
-        public void Test1()
+        public void WarriorConstructorWorksCorrectly()
         {
-            Assert.Pass();
+            Assert.AreEqual("Maximus", this.warrior.Name);
+            Assert.AreEqual(100, this.warrior.Damage);
+            Assert.AreEqual(100, this.warrior.HP);
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("\t")]
+        [TestCase("\n")]
+        [TestCase(" ")]
+        [TestCase("     ")]
+        [TestCase("")]
+        public void NameSetterThrowsArgumentsExceptionWhenParameterValueIsNullOrWhitespace(string name)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior(name, 100, 100));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void DamageSetterThrowsArgumentsExceptionWhenParameterValueIsZeroOrNegative(int damage)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("Maximus", damage, 100));
+        }
+
+        [Test]
+        [TestCase(-1)]
+        public void HPSetterThrowsArgumentsExceptionWhenParameterValueIsNegative(int healthPoints)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("Maximus", 100, healthPoints));
+        }
+
+        [Test]
+        [TestCase(100, 100, 0, 0)]
+        [TestCase(200, 50, 50, 100)]
+        [TestCase(99, 20, 80, 0)]
+        public void AttackDecreasesAttackingAndAttackedWarriorsHPCorrectly(
+            int attackedWarriorHP, 
+            int attackedWarriorDamage, 
+            int attackingWarriorExpectedHP,
+            int attackedWarriorExpectedHP)
+        {
+            Warrior attackedWarrior = new Warrior("Commodus", attackedWarriorDamage, attackedWarriorHP);
+
+            this.warrior.Attack(attackedWarrior);
+
+            Assert.AreEqual(attackingWarriorExpectedHP, this.warrior.HP);
+            Assert.AreEqual(attackedWarriorExpectedHP, attackedWarrior.HP);
+        }
+
+        [Test]
+        [TestCase(30)]
+        [TestCase(29)]
+        public void WarriorAttackWith30HPOrLessThrowsInvalidOperationException(int healthPoints)
+        {
+            Warrior attackingWarrior = new Warrior("Max", 50000, healthPoints);
+            Warrior attackedWarrior = new Warrior("Com", 50, 5000);
+
+            Assert.Throws<InvalidOperationException>(() => attackingWarrior.Attack(attackedWarrior));
+        }
+
+        [Test]
+        [TestCase(30)]
+        [TestCase(29)]
+        public void AttackedWarriorWith30HPOrLessThrowsInvalidOperationException(int healthPoints)
+        {
+            Warrior attackingWarrior = new Warrior("Max", 50, 5000);
+            Warrior attackedWarrior = new Warrior("Com", 50, healthPoints);
+
+            Assert.Throws<InvalidOperationException>(() => attackingWarrior.Attack(attackedWarrior));
+        }
+
+        [Test]
+        public void AttackWarriorTargetWithMoreDamageThanTheAttackingWarriorHPThrowsInvalidOperationException()
+        {
+            Warrior attackingWarrior = new Warrior("Max", 50, 50);
+            Warrior attackedWarrior = new Warrior("Com", 51, 500);
+
+            Assert.Throws<InvalidOperationException>(() => attackingWarrior.Attack(attackedWarrior));
         }
     }
 }
