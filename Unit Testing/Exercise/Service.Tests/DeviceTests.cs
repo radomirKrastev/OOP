69cf39d4dadb44b180dde4169d64f829e431024a
radomirKrastev <radomirkostantinov@gmail.com>
2019-11-25 23:24:23 +0200

    unit test

--- Unit Testing/Exercise/Service.Tests/DeviceTests.cs
+++ Unit Testing/Exercise/Service.Tests/DeviceTests.cs
@@ -1,18 +1,237 @@
-﻿using NUnit.Framework;
-
-namespace Service.Tests
+﻿namespace Service.Tests
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using Models.Contracts;
+    using Models.Devices;
+    using NUnit.Framework;
+
+    [TestFixture]
     public class DeviceTests
     {
+        private Assembly partAssembly;
+
         [SetUp]
         public void Setup()
         {
+            this.partAssembly = typeof(Device).Assembly;
         }
 
         [Test]
-        public void Test1()
+        [TestCase("Laptop", "Lenovo")]
+        [TestCase("PC", "HP")]
+        [TestCase("Phone", "Samsung")]
+        public void DeviceConstructorWorksCorrectly(string deviceTypeString, string deviceMake)
         {
-            Assert.Pass();
+            Type deviceType = this.partAssembly.GetTypes().First(x => x.Name == deviceTypeString);
+
+            IRepairable deviceInstance = (IRepairable)Activator.CreateInstance(deviceType, deviceMake);
+
+            Assert.AreEqual(deviceMake, deviceInstance.Make);
+            Assert.IsNotNull(deviceInstance.Parts);
+            Assert.AreEqual(0, deviceInstance.Parts.Count);
+        }
+
+        [Test]
+        [TestCase("Laptop", "")]
+        [TestCase("Laptop", null)]
+        [TestCase("PC", "")]
+        [TestCase("PC", null)]
+        [TestCase("Phone", "")]
+        [TestCase("Phone", null)]
+        public void DeviceMakeSetterThrowsArgumentExceptionIfParameterValueIsNullOrEmpty(
+            string deviceTypeString,
+            string deviceMake)
+        {
+            Type deviceType = this.partAssembly.GetTypes().First(x => x.Name == deviceTypeString);
+
+            try
+            {
+                Activator.CreateInstance(deviceType, deviceMake);
+            }
+            catch (TargetInvocationException e)
+            {
+                Assert.AreEqual(typeof(ArgumentException), e.InnerException.GetType());
+            }
+        }
+
+        [Test]
+        [TestCase("Laptop", "Lenovo", "LaptopPart")]
+        [TestCase("PC", "HP", "PCPart")]
+        [TestCase("Phone", "Samsung", "PhonePart")]
+        public void AddPartFunctionalityWorksCorrectly(string deviceTypeString, string deviceMake, string partTypeString)
+        {
+            Type deviceType = this.partAssembly.GetTypes().First(x => x.Name == deviceTypeString);
+            IRepairable deviceInstance = (IRepairable)Activator.CreateInstance(deviceType, deviceMake);
+            Type partType = this.partAssembly.GetTypes().First(x => x.Name == partTypeString);
+            IPart firstPart = (IPart)Activator.CreateInstance(partType, "processor", 350.97m, false);
+            IPart secondPart = (IPart)Activator.CreateInstance(partType, "someOtherpart", 31220.97m, true);
+            List<IPart> partsListExpected = new List<IPart> { firstPart, secondPart };
+
+            deviceInstance.AddPart(firstPart);
+            deviceInstance.AddPart(secondPart);
+
+            Assert.IsTrue(partsListExpected.SequenceEqual(deviceInstance.Parts));
+        }
+
+        [Test]
+        [TestCase("Laptop", "Lenovo", "LaptopPart")]
+        [TestCase("PC", "HP", "PCPart")]
+        [TestCase("Phone", "Samsung", "PhonePart")]
+        public void AddAlreadyExistingPartThrowsInvalidOperationException(
+            string deviceTypeString,
+            string deviceMake,
+            string partTypeString)
+        {
+            Type deviceType = this.partAssembly.GetTypes().First(x => x.Name == deviceTypeString);
+            IRepairable deviceInstance = (IRepairable)Activator.CreateInstance(deviceType, deviceMake);
+            Type partType = this.partAssembly.GetTypes().First(x => x.Name == partTypeString);
+            IPart part = (IPart)Activator.CreateInstance(partType, "processor", 350.97m, false);
+            deviceInstance.AddPart(part);
+
+            Assert.Throws<InvalidOperationException>(() => deviceInstance.AddPart(part));
+        }
+
+        [Test]
+        [TestCase("Laptop", "Lenovo", "PCPart")]
+        [TestCase("PC", "HP", "LaptopPart")]
+        [TestCase("Phone", "Samsung", "LaptopPart")]
+        public void AddWrongPartTypeThrowsInvalidOperationException(
+            string deviceTypeString,
+            string deviceMake,
+            string partTypeString)
+        {
+            Type deviceType = this.partAssembly.GetTypes().First(x => x.Name == deviceTypeString);
+            IRepairable deviceInstance = (IRepairable)Activator.CreateInstance(deviceType, deviceMake);
+            Type partType = this.partAssembly.GetTypes().First(x => x.Name == partTypeString);
+            IPart part = (IPart)Activator.CreateInstance(partType, "processor", 350.97m, false);
+
+            Assert.Throws<InvalidOperationException>(() => deviceInstance.AddPart(part));
+        }
+
+        [Test]
+        [TestCase("Laptop", "Lenovo", "LaptopPart")]
+        [TestCase("PC", "HP", "PCPart")]
+        [TestCase("Phone", "Samsung", "PhonePart")]
+        public void RemovePartFunctionalityWorksCorrectly(string deviceTypeString, string deviceMake, string partTypeString)
+        {
+            Type deviceType = this.partAssembly.GetTypes().First(x => x.Name == deviceTypeString);
+            IRepairable deviceInstance = (IRepairable)Activator.CreateInstance(deviceType, deviceMake);
+            Type partType = this.partAssembly.GetTypes().First(x => x.Name == partTypeString);
+            IPart firstPart = (IPart)Activator.CreateInstance(partType, "processor", 350.97m, false);
+            IPart secondPart = (IPart)Activator.CreateInstance(partType, "someOtherpart", 31220.97m, true);
+            List<IPart> expectedParts = new List<IPart> { firstPart };
+
+            deviceInstance.AddPart(firstPart);
+            deviceInstance.AddPart(secondPart);
+            deviceInstance.RemovePart(secondPart.Name);
+
+            Assert.IsTrue(expectedParts.SequenceEqual(deviceInstance.Parts));
+        }
+
+        [Test]
+        [TestCase("Laptop", "Lenovo", "")]
+        [TestCase("Laptop", "Lenovo", null)]
+        [TestCase("PC", "HP", "")]
+        [TestCase("PC", "HP", null)]
+        [TestCase("Phone", "Samsung", "")]
+        [TestCase("Phone", "Samsung", null)]
+        public void RemovePartWithNullOrEmptyNameThrowsArgumentException(
+            string deviceTypeString,
+            string deviceMake,
+            string partName)
+        {
+            Type deviceType = this.partAssembly.GetTypes().First(x => x.Name == deviceTypeString);
+            IRepairable deviceInstance = (IRepairable)Activator.CreateInstance(deviceType, deviceMake);
+
+            Assert.Throws<ArgumentException>(() => deviceInstance.RemovePart(partName));
+        }
+
+        [Test]
+        [TestCase("Laptop", "Lenovo")]
+        [TestCase("PC", "HP")]
+        [TestCase("Phone", "Samsung")]
+        public void RemovePartWhichHasNotBeenPreviouslyAddedThrowsInvalidOperationException(
+            string deviceTypeString,
+            string deviceMake)
+        {
+            Type deviceType = this.partAssembly.GetTypes().First(x => x.Name == deviceTypeString);
+            IRepairable deviceInstance = (IRepairable)Activator.CreateInstance(deviceType, deviceMake);
+
+            Assert.Throws<InvalidOperationException>(() => deviceInstance.RemovePart("CPU"));
+        }
+
+        [Test]
+        [TestCase("Laptop", "Lenovo", "LaptopPart")]
+        [TestCase("PC", "HP", "PCPart")]
+        [TestCase("Phone", "Samsung", "PhonePart")]
+        public void RepairPartFunctionalityWorksCorrectly(
+            string deviceTypeString,
+            string deviceMake,
+            string partTypeString)
+        {
+            Type deviceType = this.partAssembly.GetTypes().First(x => x.Name == deviceTypeString);
+            IRepairable deviceInstance = (IRepairable)Activator.CreateInstance(deviceType, deviceMake);
+            Type partType = this.partAssembly.GetTypes().First(x => x.Name == partTypeString);
+            IPart part = (IPart)Activator.CreateInstance(partType, "processor", 350.97m, true);
+            deviceInstance.AddPart(part);
+
+            deviceInstance.RepairPart(part.Name);
+            List<IPart> deviceParts = (List<IPart>)deviceInstance.Parts;
+
+            Assert.AreEqual(false, deviceParts[0].IsBroken);
+        }
+
+        [Test]
+        [TestCase("Laptop", "Lenovo", "")]
+        [TestCase("Laptop", "Lenovo", null)]
+        [TestCase("PC", "HP", "")]
+        [TestCase("PC", "HP", null)]
+        [TestCase("Phone", "Samsung", "")]
+        [TestCase("Phone", "Samsung", null)]
+        public void RepairPartWithNullOrEmptyNameThrowsArgumentException(
+            string deviceTypeString,
+            string deviceMake,
+            string partName)
+        {
+            Type deviceType = this.partAssembly.GetTypes().First(x => x.Name == deviceTypeString);
+            IRepairable deviceInstance = (IRepairable)Activator.CreateInstance(deviceType, deviceMake);
+
+            Assert.Throws<ArgumentException>(() => deviceInstance.RepairPart(partName));
+        }
+
+        [Test]
+        [TestCase("Laptop", "Lenovo")]
+        [TestCase("PC", "HP")]
+        [TestCase("Phone", "Samsung")]
+        public void RepairPartWhichHasNotBeenPreviouslyAddedThrowsInvalidOperationException(
+            string deviceTypeString,
+            string deviceMake)
+        {
+            Type deviceType = this.partAssembly.GetTypes().First(x => x.Name == deviceTypeString);
+            IRepairable deviceInstance = (IRepairable)Activator.CreateInstance(deviceType, deviceMake);
+
+            Assert.Throws<InvalidOperationException>(() => deviceInstance.RepairPart("CPU"));
+        }
+
+        [Test]
+        [TestCase("Laptop", "Lenovo", "LaptopPart")]
+        [TestCase("PC", "HP", "PCPart")]
+        [TestCase("Phone", "Samsung", "PhonePart")]
+        public void NotBrokenPartCannotBeRepairedRepairPartThrowsInvalidOperationException(
+            string deviceTypeString,
+            string deviceMake,
+            string partTypeString)
+        {
+            Type deviceType = this.partAssembly.GetTypes().First(x => x.Name == deviceTypeString);
+            IRepairable deviceInstance = (IRepairable)Activator.CreateInstance(deviceType, deviceMake);
+            Type partType = this.partAssembly.GetTypes().First(x => x.Name == partTypeString);
+            IPart part = (IPart)Activator.CreateInstance(partType, "processor", 350.97m, false);
+            deviceInstance.AddPart(part);
+
+            Assert.Throws<InvalidOperationException>(() => deviceInstance.RepairPart(part.Name));
         }
     }
 }
--- Unit Testing/Exercise/Service.Tests/PartTests.cs
+++ Unit Testing/Exercise/Service.Tests/PartTests.cs
@@ -1,18 +1,160 @@
-using NUnit.Framework;
+namespace Service.Tests
+{   
+    using System;
+    using System.Linq;
+    using System.Reflection;    
+    using Models.Contracts;
+    using Models.Parts;
+    using NUnit.Framework;
 
-namespace Tests
-{
+    [TestFixture]
     public class PartTests
     {
+        private decimal partCostMultiplier = 1;
+        private Assembly partAssembly;
+
         [SetUp]
         public void Setup()
         {
+            this.partAssembly = typeof(Part).Assembly;
         }
 
         [Test]
-        public void Test1()
+        [TestCase("LaptopPart", "processor", 1000, true)]
+        [TestCase("PCPart", "fan", 155.5, true)]
+        [TestCase("PhonePart", "touchscreen", 357, false)]
+        public void PartConstructorWorksCorrectlyWithThreeParameters(
+            string partTypeString,
+            string partName,
+            decimal partCost,
+            bool isPartBroken)
         {
-            Assert.Pass();
+            Type partType = this.partAssembly.GetTypes().First(x => x.Name == partTypeString);
+
+            IPart partInstance = (IPart)Activator.CreateInstance(partType, partName, partCost, isPartBroken);
+            this.GetMultiplier(partType, partInstance);
+
+            Assert.AreEqual(partName, partInstance.Name);
+            Assert.AreEqual(partCost * this.partCostMultiplier, partInstance.Cost);
+            Assert.AreEqual(isPartBroken, partInstance.IsBroken);
+        }
+
+        [Test]
+        [TestCase("LaptopPart", "processor", 1000)]
+        [TestCase("PCPart", "fan", 155.5)]
+        [TestCase("PhonePart", "touchscreen", 357)]
+        public void PartConstructorWorksCorrectlyWithTwoParameters(
+            string partTypeString,
+            string partName,
+            decimal partCost)
+        {
+            Type partType = this.partAssembly.GetTypes().First(x => x.Name == partTypeString);
+
+            IPart partInstance = (IPart)Activator.CreateInstance(partType, partName, partCost);
+            this.GetMultiplier(partType, partInstance);
+                        
+            Assert.AreEqual(partName, partInstance.Name);
+            Assert.AreEqual(partCost * this.partCostMultiplier, partInstance.Cost);
+            Assert.AreEqual(false, partInstance.IsBroken);
+        }
+
+        [Test]
+        [TestCase("LaptopPart", "", 1000, true)]
+        [TestCase("LaptopPart", null, 1000, true)]
+        [TestCase("PCPart", "", 155.5, true)]
+        [TestCase("PCPart", null, 155.5, true)]
+        [TestCase("PhonePart", "", 357, false)]
+        [TestCase("PhonePart", null, 357, false)]
+        public void PartNameSetterThrowsArgumentExceptionIfParameterValueIsNullOrEmpty(
+            string partTypeString,
+            string partName,
+            decimal partCost,
+            bool isPartBroken)
+        {
+            Type partType = this.partAssembly.GetTypes().First(x => x.Name == partTypeString);
+
+            try
+            {
+                Activator.CreateInstance(partType, partName, partCost, isPartBroken);
+            }
+            catch (TargetInvocationException e)
+            {
+                Assert.AreEqual(typeof(ArgumentException), e.InnerException.GetType());
+            }
+        }
+
+        [Test]
+        [TestCase("LaptopPart", "processor", 0, true)]
+        [TestCase("LaptopPart", "processor", -1, true)]
+        [TestCase("PCPart", "fan", 0, true)]
+        [TestCase("PCPart", "fan", -1, true)]
+        [TestCase("PhonePart", "touchscreen", 0, false)]
+        [TestCase("PhonePart", "touchscreen", -1, false)]
+        public void PartCostSetterThrowsArgumentExceptionIfParameterValueIsZeroOrNegative(
+            string partTypeString,
+            string partName,
+            decimal partCost,
+            bool isPartBroken)
+        {
+            Type partType = this.partAssembly.GetTypes().First(x => x.Name == partTypeString);
+
+            try
+            {
+                Activator.CreateInstance(partType, partName, partCost, isPartBroken);
+            }
+            catch (TargetInvocationException e)
+            {             
+                Assert.AreEqual(typeof(ArgumentException), e.InnerException.GetType());
+            }
+        }
+
+        [Test]
+        [TestCase("LaptopPart", "processor", 1000, true)]
+        [TestCase("PCPart", "fan", 155.5, true)]
+        [TestCase("PhonePart", "touchscreen", 357, true)]
+        public void RepairSetsIsBrokenProperyToFalse(
+            string partTypeString,
+            string partName,
+            decimal partCost,
+            bool isPartBroken)
+        {
+            Type partType = this.partAssembly.GetTypes().First(x => x.Name == partTypeString);
+            IPart partInstance = (IPart)Activator.CreateInstance(partType, partName, partCost, isPartBroken);
+
+            partInstance.Repair();
+
+            Assert.AreEqual(false, partInstance.IsBroken);
+        }
+
+        [Test]
+        [TestCase("LaptopPart", "processor", 1000, true)]
+        [TestCase("PCPart", "fan", 155.5, false)]
+        [TestCase("PhonePart", "touchscreen", 357, false)]
+        public void ReportReturnsCorrectlyInformationAboutThePartAsString(
+            string partTypeString,
+            string partName,
+            decimal partCost,
+            bool isPartBroken)
+        {
+            Type partType = this.partAssembly.GetTypes().First(x => x.Name == partTypeString);
+            IPart partInstance = (IPart)Activator.CreateInstance(partType, partName, partCost, isPartBroken);
+            this.GetMultiplier(partType, partInstance);
+            string expectedReportMessage = $"{partName} - {partCost * partCostMultiplier:f2}$"
+                + Environment.NewLine
+                + $"Broken: {isPartBroken}";
+
+            Assert.AreEqual(expectedReportMessage, partInstance.Report());
+        }
+
+        private void GetMultiplier(Type partType, IPart partInstance)
+        {
+            if (partType != typeof(Part))
+            {
+                this.partCostMultiplier = (decimal)partType.GetFields(BindingFlags.Static | BindingFlags.NonPublic)
+                .Where(fi => fi.IsInitOnly)
+                .First()
+                .GetValue(partInstance);
+            }
         }
     }
 }
