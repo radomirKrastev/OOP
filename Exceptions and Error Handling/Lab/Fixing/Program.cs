503c51d5edf56d8b330b6670f02c03807ee5190d
radomirKrastev <radomirkostantinov@gmail.com>
2019-11-26 15:30:45 +0200

    exceptions

---
+++ Exceptions and Error Handling/Lab/Fixing/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Fixing
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] weekdays = new string[5];
+            weekdays[0] = "Sunday";
+            weekdays[1] = "Monday";
+            weekdays[2] = "Tuesday";
+            weekdays[3] = "Wednesday";
+            weekdays[4] = "Thursday";
+
+            try
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine(weekdays[i].ToString());
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
