9da64ef933fdda723b6b6cf3ef8acb0471a4aaf5
radomirKrastev <radomirkostantinov@gmail.com>
2019-11-26 15:09:56 +0200

    exceptions

---
+++ Exceptions and Error Handling/Lab/SquareRoot/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SquareRoot
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number < 0)
+                {
+                    throw new ArgumentException();
+                }
+
+                int squareRootNumber = (int)Math.Sqrt(number);
+                Console.WriteLine(squareRootNumber);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Invalid number");                    
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
