6fc2e8cc182ff3474592e6256005936c100a600f
radomirKrastev <radomirkostantinov@gmail.com>
2019-11-26 16:45:44 +0200

    exceptions

---
+++ Exceptions and Error Handling/Lab/ConvertToDouble/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConvertToDouble
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            try
+            {
+                double number = Convert.ToDouble(input);
+                Console.WriteLine(number*2);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine(typeof(OverflowException).Name);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(typeof(FormatException).Name);
+            }
+            catch (InvalidCastException)
+            {
+                Console.WriteLine(typeof(InvalidCastException).Name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
