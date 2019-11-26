a693ebc99ed878c480876393f6aeafb6ae857e5b
radomirKrastev <radomirkostantinov@gmail.com>
2019-11-26 16:06:14 +0200

    exceptions

---
+++ Exceptions and Error Handling/Lab/Fixing2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Fixing2
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int num1, num2;
+            byte result;
+            
+            num1 = 30;
+            num2 = 60;
+
+            try
+            {
+                result = Convert.ToByte(num1 * num2);
+                Console.WriteLine("{0} * {1} = {2}", num1, num2, result);
+            }
+            catch (OverflowException)
+            {
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
