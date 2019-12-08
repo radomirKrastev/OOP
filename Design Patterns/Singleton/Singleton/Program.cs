7add99345421ac1c2fc086f5e0159318638c9d2d
radomirKrastev <radomirkostantinov@gmail.com>
2019-12-08 11:44:46 +0200

    Singleton design pattern

---
+++ Design Patterns/Singleton/Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            SingletonDataContainer db = SingletonDataContainer.Instance;
+            Console.WriteLine(db.GetPopulation("Capital City"));
+
+            SingletonDataContainer db1 = SingletonDataContainer.Instance;
+            Console.WriteLine(db.GetPopulation("Pliska"));
+
+            SingletonDataContainer db2 = SingletonDataContainer.Instance;
+            Console.WriteLine(db.GetPopulation("London"));
+
+            SingletonDataContainer db3 = SingletonDataContainer.Instance;
+            Console.WriteLine(db.GetPopulation("Mumbai"));
+        }
+    }
+}
